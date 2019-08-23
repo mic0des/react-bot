c2ad92a2ef8ff907254071fef959e4b72b32500a
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-22 23:50:54 -0400

    finish eventQuery method

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,5 +1,6 @@
 'use strict'
 const dialogflow = require('dialogflow');
+const structjson = require('./structjson');
 const config = require('../config/keys');
 
 const sessionClient = new dialogflow.SessionsClient();
@@ -21,6 +22,23 @@ module.exports = {
         payload: {
           data: parameters
         }
+      }
+    };
+    let responses = await sessionClient
+      .detectIntent(request);
+    responses = await self.handleAction(responses)
+    return responses;
+  },
+  eventQuery: async function(event, parameters = {}) {
+    let self = module.exports;
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        event: {
+          name: event,
+          parameters: structjson.jsonToStructProto(parameters),
+          languageCode: config.dialogFlowSessionLanguageCode,
+        },
       }
     };
     let responses = await sessionClient
