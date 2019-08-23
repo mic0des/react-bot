b75c44d3773fd8e5fd33345fb0be06952e11cd8e
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-23 00:21:14 -0400

    add sessions config dependency based on updated keys file

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -2,8 +2,14 @@
 const dialogflow = require('dialogflow');
 const structjson = require('./structjson');
 const config = require('../config/keys');
+const projectID = config.googleProjectID;
 
-const sessionClient = new dialogflow.SessionsClient();
+const credentials = {
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey
+}
+
+const sessionClient = new dialogflow.SessionsClient({projectID, credentials});
 
 const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
