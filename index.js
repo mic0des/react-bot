be985ea166ee05a4997b0f6982436dd5a107d628
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-22 20:02:29 -0400

    add dialogflow route skeletons

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send({'hello': 'there'});
 });
 
+app.post('/api/df_text_query', (req, res) => {
+  res.send({'do': 'text query'})
+});
+
+app.post('/api/df_event_query', (req, res) => {
+  res.send({'do': 'event query'})
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
