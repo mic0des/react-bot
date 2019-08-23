b9527696025d9d41de1bd2f237424e06b36196fa
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-22 20:14:10 -0400

    extract logic to their own directory and files

--- index.js
+++ index.js
@@ -3,18 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send({'hello': 'there'});
-});
-
-app.post('/api/df_text_query', (req, res) => {
-  res.send({'do': 'text query'})
-});
-
-app.post('/api/df_event_query', (req, res) => {
-  res.send({'do': 'event query'})
-});
+require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
