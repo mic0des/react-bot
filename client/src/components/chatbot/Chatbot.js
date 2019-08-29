62e63b1d34896e3409ab8a0af039b86cf94c018a
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-29 17:14:44 -0400

    bot can send and receive messages

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -6,6 +6,8 @@ class Chatbot extends Component {
   constructor(props){
     super(props);
 
+    this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+
     this.state = {
       messages: []
     }
@@ -23,7 +25,7 @@ class Chatbot extends Component {
     this.setState({messages: [...this.state.messages, says]});
     const res = await axios.post('/api/df_text_query', {text});
 
-    for (let msg of res.data.fullfillmentMessages) {
+    for (let msg of res.data.fulfillmentMessages) {
       let says = {
         speaks: 'bot',
         msg: msg
@@ -58,13 +60,20 @@ class Chatbot extends Component {
     }
   }
 
+  _handleInputKeyPress(e){
+    if (e.key === 'Enter') {
+      this.df_text_query(e.target.value);
+      e.target.value = '';
+    }
+  }
+
   render() {
     return (
       <div style={{height: 400, width: 400, float: 'right'}}>
         <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
           <h2>Chatbot</h2>
           {this.renderMessages(this.state.messages)}
-          <input type='text' />
+          <input type='text' onKeyPress={this._handleInputKeyPress} />
         </div>
       </div>
     )
