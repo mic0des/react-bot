971c9e1fedfe128921794e8538287bda53a238bd
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-29 14:19:20 -0400

    create chatbot component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import Landing from './pages/Landing';
 import About from './pages/About';
 import Shop from './shop/Shop';
 import Header from './Header';
+import Chatbot from './chatbot/Chatbot';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route exact path="/" component={Landing}/>
           <Route exact path="/about" component={About}/>
           <Route exact path="/shop" component={Shop}/>
+          <Chatbot/>
         </div>
       </BrowserRouter>
     </div>
