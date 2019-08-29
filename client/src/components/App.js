4bb1dae21622d4a60199fc1c80a708db766c23f8
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-29 14:05:53 -0400

    add Header component to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import About from './pages/About';
 import Shop from './shop/Shop';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <Header/>
           <Route exact path="/" component={Landing}/>
           <Route exact path="/about" component={About}/>
           <Route exact path="/shop" component={Shop}/>
