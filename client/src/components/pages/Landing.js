9769bad21c1e61ad0918079c1daa81d8ca1a686e
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-29 13:48:26 -0400

    create About and Landing components

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,1 +1,11 @@
-Landing.js
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div><h1>Selling you great stuff!</h1>
+    with the help of a chatbot
+    </div>
+  )
+}
+
+export default Landing;
