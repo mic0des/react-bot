915a23d6df71e640259a4ea6eeb7a12b6888480b
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2019-08-29 17:52:04 -0400

    send saved cookie to dialogflow backend routes to save chat session

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -3,6 +3,7 @@ const dialogflow = require('dialogflow');
 const structjson = require('./structjson');
 const config = require('../config/keys');
 const projectID = config.googleProjectID;
+const sessionID = config.dialogFlowSessionID;
 
 const credentials = {
   client_email: config.googleClientEmail,
@@ -11,10 +12,11 @@ const credentials = {
 
 const sessionClient = new dialogflow.SessionsClient({projectID, credentials});
 
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+// const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
 module.exports = {
-  textQuery: async function(text, parameters = {}) {
+  textQuery: async function(text, userID, parameters = {}) {
+    let sessionPath = sessionClient.sessionPath(projectID, sessionID + userID);
     let self = module.exports;
     const request = {
       session: sessionPath,
@@ -35,7 +37,8 @@ module.exports = {
     responses = await self.handleAction(responses)
     return responses;
   },
-  eventQuery: async function(event, parameters = {}) {
+  eventQuery: async function(event, userID, parameters = {}) {
+    let sessionPath = sessionClient.sessionPath(projectID, sessionID + userID);
     let self = module.exports;
     const request = {
       session: sessionPath,
